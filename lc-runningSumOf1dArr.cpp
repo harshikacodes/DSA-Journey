4e891ebde2da6ea8b665c1e1ab0b015d60d969e1
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-08-05 20:55:12 +0530

    Update lc-runningSumOf1dArr.cpp

--- lc-runningSumOf1dArr.cpp
+++ lc-runningSumOf1dArr.cpp
@@ -3,8 +3,11 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         int n = nums.size();
         vector <int> pSum;
+        // I used vector here because return type is of vector
         int sum=0;
 
+        // here i use prefix sum approach
+        // and as i have vector so i just push_back sum 
         for(int i=0; i<n; i++){
             sum = sum + nums[i];
             pSum.push_back(sum);
